1dc6c05f5e3fd4e14b94d474024962a191f1dc8c
rumeysagul <rumeysagul@users.noreply.github.com>
2026-05-21 11:59:06 +0300

    maindeğişmişsonhali

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        SystemConfiguration config = SystemConfiguration.getInstance();
+        config.setRiskLimit(4.0);
+        config.setActiveStrategy("LONG_TERM");
         TradingFacade tradingSystem = new TradingFacade();
         tradingSystem.startTrading();
     }
